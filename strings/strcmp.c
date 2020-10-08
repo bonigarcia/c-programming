99674ca9990218754a5b0e30895be28811ade024
Boni García <boni.garcia@uc3m.es>
2020-10-08 09:44:40 +0200

    Inclde simple example using strcmp

---
+++ strings/strcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "abcd", str2[] = "abCd", str3[] = "abcd";
+
+    int comparison = strcmp(str1, str2);
+    if (comparison) {
+        printf("\"%s\" and \"%s\" are DIFFERENT (comparison=%d)\n", str1, str2,
+                comparison);
+    }
+
+    comparison = strcmp(str1, str3);
+    if (!comparison) {
+        printf("\"%s\" and \"%s\" are IDENTICAL (comparison=%d)\n", str1, str3,
+                comparison);
+    }
+
+    return 0;
+}
