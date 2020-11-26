b4ef221e674dda8aacee5a802fc9a125485f56d9
Boni García <boni.garcia@uc3m.es>
2020-11-26 16:58:49 +0100

    Remove comments in Valgrind example

--- valgrind/activities/testing_valgrind.c
+++ valgrind/activities/testing_valgrind.c
@@ -2,8 +2,7 @@
 
 void f(void) {
     int *x = malloc(10 * sizeof(int));
-    x[10] = 0; // problem 1: heap block overrun
-               // problem 2: memory leak -- x not freed
+    x[10] = 0;
 }
 
 int main(void) {
