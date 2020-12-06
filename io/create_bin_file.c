4ed32e940955d599f0cca977579fed347231260e
Boni García <boni.garcia@uc3m.es>
2020-12-06 19:55:28 +0100

    Include example which writes a binary file

---
+++ io/create_bin_file.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STR 255
+#define SIZE 10
+
+struct data {
+    char str[MAX_STR];
+};
+
+int main(int argc, char *argv[]) {
+    struct data *content = (struct data*) calloc(SIZE, sizeof(struct data));
+
+    for (int i = 0; i < SIZE; i++) {
+        sprintf(content[i].str, "This is line %d", i + 1);
+    }
+
+    FILE *fp = fopen("file1.bin", "wb");
+    if (!fp) {
+        perror("An error occurred opening the file\n");
+        return 1;
+    }
+
+    fwrite(content, sizeof(struct data), SIZE, fp);
+
+    fclose(fp);
+
+    free(content);
+
+    return 0;
+}
