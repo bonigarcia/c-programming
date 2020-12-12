cf909930f9901b7a324efc224263d457a0045144
Boni García <boni.garcia@uc3m.es>
2020-12-12 10:27:40 +0100

    Avoid warnings when compiling pthread examples in Linux

--- threads/threads-2.c
+++ threads/threads-2.c
@@ -4,13 +4,11 @@
 #include <unistd.h>
 
 void* thread_run(void *data) { //sleep(2);
-    printf("[TH_1 %d]: Hello from the thread \n", pthread_self());
+    printf("[TH_1 %ld]: Hello from the thread \n", pthread_self());
     sleep(1);
     (*(int*) data)++;
-    printf("[TH_1 %d]: To exit...............\n", pthread_self());
+    printf("[TH_1 %ld]: To exit...............\n", pthread_self());
     pthread_exit(data);
-
-    return NULL;
 }
 
 int main() {
@@ -18,18 +16,18 @@ int main() {
     int data = 0;
     int thread_rc = 0;
 
-    printf("[MAIN %d]: Starting............ \n", pthread_self());
+    printf("[MAIN %ld]: Starting............ \n", pthread_self());
     if ((thread_rc = pthread_create(&thread, NULL, thread_run, &data)) != 0) {
         printf("Error creating the thread. Code %i", thread_rc);
         return -1;
     }
 
     sleep(1);
-    printf("[MAIN %d]: Thread allocated \n", pthread_self());
+    printf("[MAIN %ld]: Thread allocated \n", pthread_self());
 
     int *ptr_output_data;
     pthread_join(thread, (void**) &ptr_output_data);
-    printf("[MAIN %d]: Thread returns %d \n", pthread_self(), *ptr_output_data);
+    printf("[MAIN %ld]: Thread returns %d \n", pthread_self(), *ptr_output_data);
 
     return 0;
 }
