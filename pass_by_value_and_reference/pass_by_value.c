b25b68b84a0a30004f037db3731fa3adffffba51
Boni García <boni.garcia@uc3m.es>
2021-10-06 10:29:47 +0200

    Include examples for passing variables by value and reference

---
+++ pass_by_value_and_reference/pass_by_value.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *first, int *second) {
+    int tmp;
+
+    tmp = *first;
+    *first = *second;
+    *second = tmp;
+}
+
+int main(void) {
+    int a = 100;
+    int b = 200;
+
+    puts("Before swap:");
+    printf("\t a=%d \n", a);
+    printf("\t b=%d \n", b);
+
+    swap(&a, &b);
+
+    puts("After swap:");
+    printf("\t a=%d \n", a);
+    printf("\t b=%d \n", b);
+
+    return 0;
+}
