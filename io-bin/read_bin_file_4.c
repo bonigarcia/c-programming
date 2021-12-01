f2f26ef7454fa3c93035baf4a96f5dc1914656d7
Boni García <boni.garcia@uc3m.es>
2021-12-01 02:22:43 +0100

    Include another example for reading a binary file

---
+++ io-bin/read_bin_file_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STR 255
+#define SIZE 10
+
+int main() {
+    FILE *fp = fopen("file2.bin", "rb");
+    if (!fp) {
+        perror("An error occurred opening the file\n");
+        return 1;
+    }
+
+    printf("The content of the binary file is:\n");
+    char content[SIZE][MAX_STR];
+
+    size_t num = fread(&content, MAX_STR, SIZE, fp);
+    printf("Read %ld elements\n", num);
+
+    for (int i = 0; i < SIZE; i++) {
+        puts(content[i]);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
