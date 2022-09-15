fdf7e630a3f462c17f71c87fd7e6885cfcdff5b2
Boni Garcia <boni.garcia@uc3m.es>
2022-09-15 12:21:43 +0200

    Enhance strcmp example

--- strings/strcmp.c
+++ strings/strcmp.c
@@ -4,16 +4,18 @@
 int main() {
     char str1[] = "abcd", str2[] = "abCd", str3[] = "abcd";
 
+    // strcmp returns 0 when both string are equal
     int comparison = strcmp(str1, str2);
-    if (comparison) {
+
+    // 0 is evaluated as false in a conditional
+    if (comparison != 0) { // if (comparison) {
         printf("\"%s\" and \"%s\" are DIFFERENT (comparison=%d)\n", str1, str2,
                 comparison);
     }
 
     comparison = strcmp(str1, str3);
-    // strcmp returns 0 when both string are equal
-    // 0 is evaluated as false in a conditional
-    if (comparison == 0) {    //if (!comparison) {
+
+    if (comparison == 0) { // if (!comparison) {
         printf("\"%s\" and \"%s\" are IDENTICAL (comparison=%d)\n", str1, str3,
                 comparison);
     }
