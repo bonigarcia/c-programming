61d08bb2baa8367f1625b9237eb0c4c586cfcc13
Boni Garcia <boni.garcia@uc3m.es>
2022-09-21 18:52:42 +0200

    Improve solutions for scanf programs

---
+++ io/scanf_6_sol2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+
+    for (;;) { // Infinite loop
+        printf("Enter an option from 1 to 7 (8 for exit): ");
+        scanf("%d", &i);
+        while (getchar() != '\n'); // Clear input buffer
+
+        if (i > 0 && i < 8) {
+            printf("You entered %d\n", i);
+        } else if (i == 8) {
+            printf("Goodbye!\n");
+            break;
+        } else {
+            printf("Wrong option\n");
+        }
+    }
+
+    return 0;
+}
