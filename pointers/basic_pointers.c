79695d29b9566818a9ae97331d59634ce70e4ddc
Boni Garcia <boni.garcia@uc3m.es>
2022-09-26 14:50:37 +0200

    Include basic example using pointers

---
+++ pointers/basic_pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    int *p_age = &age;
+
+    printf("The value of the variable age is %d\n", age);
+    printf("The memory address in which age is stored is %p\n", p_age);
+    printf("The value pointed by p_age is %d\n", *p_age);
+
+    return 0;
+}
