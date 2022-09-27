e165a6a36e0ffcd7d3538928e4b512ce755c7082
Boni Garcia <boni.garcia@uc3m.es>
2022-09-27 13:44:13 +0200

    Modify hello with args program

--- hello_world/hello_args.c
+++ hello_world/hello_args.c
@@ -2,10 +2,12 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Arguments error. Usage: %s <name>\n", argv[0]);
+        fprintf(stderr, "Arguments error. Usage: %s <name>\n", argv[0]);
         return 1;
+
     } else {
         printf("Hello %s\n", argv[1]);
     }
+
     return 0;
 }
