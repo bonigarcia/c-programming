4f1ba717f7e7511352bc5f33efe39a19e6b5e048
Boni Garcia <boni.garcia@uc3m.es>
2023-09-09 17:32:16 +0200

    Enhance types examples

---
+++ types/promotion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    char c = 'A'; // The ASCII value of 'A' is 65
+    int sum;
+
+    sum = i + c;
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
--- types/typedef.c
+++ types/typedef.c
@@ -2,16 +2,8 @@
 
 int main() {
     typedef unsigned char byte;
-
-    byte a, b, c;
-
-    a = 'd';
-    b = 0x65;
-    c = 102;
-
-    printf("a=%c a=%d a=%X\n", a, a, a);
-    printf("b=%c b=%d b=%X\n", b, b, b);
-    printf("c=%c c=%d c=%X\n", c, c, c);
+    byte character = 'd';
+    printf("My character is: %c\n", character);
 
     return 0;
 }
