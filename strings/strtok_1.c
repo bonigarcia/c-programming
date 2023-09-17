899bd6715a1b3021ca5c9c8ac2a7ab65549b191f
Boni Garcia <boni.garcia@uc3m.es>
2023-09-17 18:04:04 +0200

    Include help links in strtok example

--- strings/strtok_1.c
+++ strings/strtok_1.c
@@ -3,6 +3,11 @@
 
 #define SEPARATOR " "
 
+/*
+ References:
+ https://cplusplus.com/reference/cstring/strtok/
+ https://stackoverflow.com/questions/23456374/why-do-we-use-null-in-strtok
+ */
 int main() {
     char str[80] = "Hello world";
     char *token = strtok(str, SEPARATOR);
@@ -13,3 +18,4 @@ int main() {
     }
     return 0;
 }
+
