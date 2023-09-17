a02985d9b72b79d1cf5ff749019fd1ac0d414da1
Boni Garcia <boni.garcia@uc3m.es>
2023-09-17 16:47:01 +0200

    Extend examples of variadic functions

---
+++ functions/variadic_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number = 10;
+    char character = 'a';
+
+    printf("Using printf\n");
+    printf("Number is %d\n", number);
+    printf("Number is %d and character is %c\n", number, character);
+
+    return 0;
+}
