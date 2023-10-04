6470dc13fe4a16bc5f3e721d6faa2763a7308411
Boni Garcia <boni.garcia@uc3m.es>
2023-10-04 10:47:14 +0200

    Improve example to convert string to enum

--- strings/str2enum.c
+++ strings/str2enum.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-enum key {
+typedef enum key {
     ON, OFF
-};
+} key;
 
 struct key_converter {
     enum key key;
@@ -12,7 +12,7 @@ struct key_converter {
 
 struct key_converter key_conv_arr[] = { { ON, "ON" }, { OFF, "OFF" } };
 
-enum key str2enum(const char *str) {
+key str2key(char *str) {
     for (int i = 0; i < sizeof(key_conv_arr) / sizeof(key_conv_arr[0]); i++) {
         if (!strcmp(str, key_conv_arr[i].str)) {
             return key_conv_arr[i].key;
@@ -21,12 +21,28 @@ enum key str2enum(const char *str) {
     return OFF; // Default value
 }
 
-int main() {
-    char *key_str[] = { "ON", "OFF", "OTHER" };
+char* key2str(key key) {
+    return key_conv_arr[key].str;
+}
 
-    for (int i = 0; i < sizeof(key_str) / sizeof(key_str[0]); i++) {
-        printf("--> %s %d\n", key_str[i], str2enum(key_str[i]));
-    }
+int main() {
+    // 1. Convert "ON" to enumerated type
+    char *key_str_on = "ON";
+    key key_enum_on = str2key(key_str_on);
+    printf("1. Original string: %s -- Enumeration value: %d\n", key_str_on,
+            key_enum_on);
+
+    // 2. Convert "OFF" to enumerated type
+    char *key_str_off = "OFF";
+    key key_enum_off = str2key(key_str_off);
+    printf("2. Original string: %s -- Enumeration value: %d\n", key_str_off,
+            key_enum_off);
+
+    // 3. Convert enumerated types to string
+    char *key_str_on_2 = key2str(key_enum_on);
+    char *key_str_off_2 = key2str(key_enum_off);
+    printf("3. The enumerated types, displayed as strings are: %s and %s\n",
+            key_str_on_2, key_str_off_2);
 
     return 0;
 }
