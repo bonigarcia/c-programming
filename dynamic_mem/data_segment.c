142bfec7eb00868bdeea9ff1af295e382d78ff95
Boni Garcia <boni.garcia@uc3m.es>
2024-09-17 12:32:53 +0200

    Enhance comments in data segment example

--- dynamic_mem/data_segment.c
+++ dynamic_mem/data_segment.c
@@ -1,11 +1,11 @@
-int num; // This is a global variable, stored in the data segment
+int num; // This is a global variable, stored in the BSS data segment
 
 int main() {
-    const int max = 15; // This is a constant variable, stored in the data segment
+    const int max = 15; // This is a constant variable, stored in the read-only data segment
 
-    static int min = 0; // This is a static variable, stored in the data segment
+    static int min = 0; // This is a static variable, stored in the initialized data segment
 
-    char *msg = "Hello world"; // This is a string literal, stored in the data segment
+    char *msg = "Hello world"; // This is a string literal, stored in the read-only data segment
 
 
     printf("%s\n", msg);
