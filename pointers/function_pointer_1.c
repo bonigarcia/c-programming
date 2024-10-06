8d2fe93d90925295cf8ce3a6b93a2e7efdb581c5
Boni Garcia <boni.garcia@uc3m.es>
2024-10-06 11:35:59 +0200

    Enhance comments in function pointer example

--- pointers/function_pointer_1.c
+++ pointers/function_pointer_1.c
@@ -6,10 +6,9 @@ int add(int a, int b) {
 
 int main() {
     int (*func_ptr)(int, int); // Declare a function pointer
-    func_ptr = add;       // Assign the address of 'add' function to the pointer
+    func_ptr = add; // Assign the address of 'add' function to the pointer
 
-    // Now you can call the function using the pointer
-    int result = func_ptr(5, 3);
+    int result = func_ptr(5, 3); // Call the function using the pointer
     printf("Result: %d\n", result); // Output will be 8
 
     return 0;
