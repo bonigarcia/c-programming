739d3c16e5e35ddbc25d152332638636a89e2329
Boni Garcia <boni.garcia@uc3m.es>
2024-10-20 15:31:05 +0200

    Update debug gdb program

--- debug/debug_gdb.c
+++ debug/debug_gdb.c
@@ -7,7 +7,7 @@ void printArray(int *arr, int size) {
 }
 
 int main() {
-    int numbers[] = { 10, 20, 30 };
+    int numbers[] = { 10, 20, 30, 40, 50 };
     int size = sizeof(numbers) / sizeof(numbers[0]);
 
     printf("Array contents:\n");
