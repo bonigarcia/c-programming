0b444e4d6eeb2f93a4c79b77652754e139166aa3
Boni Garcia <boni.garcia@uc3m.es>
2024-10-23 15:24:59 +0200

    Include example using fgets and sscanf

---
+++ io/scanf_6_sol3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    int bufsize = 80;
+    char buffer[bufsize];
+
+    for (;;) { // Infinite loop
+        printf("Enter an option from 1 to 7 (8 for exit): ");
+        fgets(buffer, bufsize, stdin);
+
+        if (sscanf(buffer, "%d", &i) == 1) {
+            if (i > 0 && i < 8) {
+                printf("You entered %d\n", i);
+            } else if (i == 8) {
+                puts("Goodbye!");
+                break;
+            }
+        } else {
+            puts("Wrong option");
+        }
+    }
+
+    return 0;
+}
